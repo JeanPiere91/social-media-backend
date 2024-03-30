823cda9f89bc601e77cc48681bb0f0a68ffcb9ae
Jean Piere Bellota Mendoza <jeanpiere.bellota@gmail.com>
2024-03-31 00:46:03 +1100

    Set folders, models and first API to get users

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: () => Promise.resolve(false),
+        message: 'Email validation failed'
+      }
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
